cc6b8004959f0b2e341704b9a18fd633c314faaf
DanielMontoliuLlopis <103653854+DanielMontoliuLlopis@users.noreply.github.com>
2023-12-04 22:52:51 +0100

    Create Day4_1.java

---
+++ Day4/Day4_1.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Day4_1 {
+    static  String URL = "input.txt";
+    public static void main(String[] args) {
+        List<String> allData = loadData();
+        List<String> numbersData = new ArrayList<>();
+        allData.forEach(line -> numbersData.add(line.split(":")[1]));
+
+        int sum = 0;
+
+        for (String data: numbersData) {
+            data = data.replaceAll("  ", " ").trim();
+            List<String> winners = new ArrayList<>(List.of(data.split("\\|")[0].split(" ")));
+            List<String> yourNumbers = List.of(data.split("\\|")[1].split(" "));
+
+
+            winners.retainAll(yourNumbers);
+            int numToPlus = 0;
+            for (int i = 0; i < winners.size(); i++) {
+                if(numToPlus == 0) {
+                    numToPlus = 1;
+                    continue;
+                }
+                numToPlus *= 2;
+
+            }
+            sum += numToPlus;
+        }
+        System.out.println(sum);
+    }
+    static List<String> loadData () {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(URL));
+            List<String> list = new ArrayList<>();
+            try {
+                String line = br.readLine();
+
+                while (line != null) {
+                    list.add(line);
+                    line = br.readLine();
+                }
+
+            } finally {
+                br.close();
+            }
+            return list;
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return List.of();
+    }
+    static List<String> loadDemoData() {
+        return List.of("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11");
+    }
+}
